0578402b99a4b9eef58f82cb7690a8e8ea721c74
Trent Butler <redtrentbutler@gmail.com>
2018-06-06 14:07:21 -0500

    guide

---
+++ Roslyn-Guide/SolutionName/ProjectName/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+/*
+To test the installed Roslyn API we will go over a simple implementation where we will: 
+Create a custom class that stores a string.
+Attempt to evaluate a string as code (“var custom_class = new CustomClass(“data”)”) or (“10 * 20”), etc.
+After evaluating, output to a console window the result
+*/
+
+namespace ProjectName
+{
+    class Class1
+    {
+        public string Data { get; set; } //STRING AUTO PROPTERY
+
+        public Class1(string data) //CLASS INITIALIZER
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return Data;
+        }
+    }
+}
